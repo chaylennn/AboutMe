189601c111a034bd57abac3f0f17c0f60f3b5420
Chaylen <chaylennguyentran@gmail.com>
2025-07-23 13:23:30 -0700

    add color and background; changed text

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -8,15 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+        ZStack{
+            Color(.systemPink).ignoresSafeArea()
+            
+            
+            VStack {
+                
+                
+                
+                Text("About Me")
+                    .font(.title)
+                    .fontWeight(.bold).padding()
+                Image("IMG_0386")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                HStack{
+                    
+                    Text("Chaylen Nguyen-Tran")
+                        .font(.headline)
+                    Text("An aspiring programmer interested in all things STEM and creative")
+                }
+                
+            
+                
+                
+                
+            }
+            
         }
         .padding()
+        .background(Rectangle().foregroundColor(.white))
+        .cornerRadius(15)
+        .shadow(radius: 15)
+        
+        ZStack{
+            Color(.systemBlue).ignoresSafeArea()
+            
+            
+            VStack {
+                
+                Spacer()
+        
+                Image("crochetsea")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                HStack{
+                    
+    
+                    Text("Chaylen loves to crochet and loves the sea").padding()
+                    
+                }
+                
+                
+            }
+            
+        }
+        .padding()
+        .background(Rectangle().foregroundColor(.white))
+        .cornerRadius(15)
+        .shadow(radius: 15)
+
+
     }
+
 }
 
 #Preview {
